061e156a9ff47c2370e4f329de477b271d85502c
Tarun Karthick G <gtarungopi@gmail.com>
2026-09-26 15:11:58 +0530

    Time: 3 ms (85.98%), Space: 43.3 MB (40.94%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch == '('|| ch == '['|| ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char t =st.pop();
+                if(ch==')' && t !='('){
+                    return false;
+                }
+                 if(ch==']' && t !='['){
+                    return false;
+                 }
+                  if(ch=='}' && t !='{'){
+                    return false;
+                  }
+            }
+           
+     }
+        
+        return st.isEmpty();
+    }
+}
